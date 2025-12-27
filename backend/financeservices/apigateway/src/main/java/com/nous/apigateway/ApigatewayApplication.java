ea0c9d32d9fd94d86e45cc49db58d4c2dc39e03d
Nousheen Ahmad <36047888+nousheenkiot@users.noreply.github.com>
2025-12-27 20:32:36 +0530

    feat: Add Apigateway service with Spring Cloud Discovery and config server integration.

--- backend/financeservices/apigateway/src/main/java/com/nous/apigateway/ApigatewayApplication.java
+++ backend/financeservices/apigateway/src/main/java/com/nous/apigateway/ApigatewayApplication.java
@@ -1,4 +1,4 @@
-package com.noush.apigateway;
+package com.nous.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
